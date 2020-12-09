c8874e1a9025cbf5f476d0bcd77ffe71b9e593ea
Ankur <ankurnahar.an@gmail.com>
2020-12-09 11:56:38 +0600

    user controller

---
+++ api/controllers/user.js
@@ -0,0 +1,27 @@
+const userService = require('../../services/user');
+
+const loginUser = async (req, res) => {
+
+    const userInfo = await userService.loginUser(req.body);
+    return res.status(userInfo.status).json(userInfo);
+}
+
+const signupUser = async (req, res) => {
+
+    const userSignup = await userService.signupUser(req.body);
+    return res.status(userSignup.status).json(userSignup);
+}
+
+const logoutUser = async (req, res) => {
+
+    const userLogout = await userService.logoutUser(req.body);
+    return res.status(userLogout.status);
+}
+
+
+
+module.exports = {
+    loginUser,
+    signupUser,
+    logoutUser
+}
